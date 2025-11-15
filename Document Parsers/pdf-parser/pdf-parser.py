48ea09112a1b531e159af8d008926eab14b7d550
Ericson Castasus <castasuse@gmail.com>
2025-11-15 10:33:54 +0800

    Create first agent in LangGraph

--- Document Parsers/pdf-parser/pdf-parser.py
+++ Document Parsers/pdf-parser/pdf-parser.py
@@ -1,12 +1,1 @@
-""" 
-This code will use PyPDFParser package
-https://py-pdf-parser.readthedocs.io/en/latest/overview.html 
 
-"""
-
-from py_pdf_parser.loaders import load_file
-from py_pdf_parser.visualise import visualise
-
-document = load_file("simple_memo.pdf")
-
-visualise(document)
